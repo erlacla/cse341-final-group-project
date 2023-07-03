2d90a78bded560a4015f61cb3860e9367922a544
Antonio Saucedo <97267913+Antonio-Saucedo@users.noreply.github.com>
2023-07-03 11:33:19 -0600

    Update index.js
    Adding route controller connections to uncomment when controller is setup.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ routes.get("/", (req, res) => {
 });
 
 routes.use("/", require("./swagger"));
+routes.use("/", require("./get"));
 routes.use("/", require("./post"));
+// routes.use("/", require("./put"));
+// routes.use("/", require("./delete"));
 
 module.exports = routes;
