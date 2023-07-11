a6b4322efdaa1935bb5c11dac7ae49fcb125c7e7
Antonio-Saucedo <antoniojesus@byui.edu>
2023-07-11 14:58:05 -0600

    Uncomment delete route

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ routes.use("/", require("./swagger"));
 routes.use("/", require("./get"));
 routes.use("/", require("./post"));
 routes.use("/", require("./put"));
-// routes.use("/", require("./delete"));
+routes.use("/", require("./delete"));
 
 module.exports = routes;
